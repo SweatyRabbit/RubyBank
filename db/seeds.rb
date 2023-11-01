4c92de6b498d5600b993473dbbc04c28389c5d01
Ivan Hulko <ivan_hulko@epam.com>
2023-11-01 22:42:12 +0200

    refactor code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-user = FactoryBot.create(:user, email: 'example@gmail.com', password: '123123')
-FactoryBot.create(:credit_card, user:)
+user = User.first_or_create(email: 'example@gmail.com', password: '123123')
+CreditCard.create(number: FFaker::Bank.card_number, balance: rand(10_000..100_000), user:)
