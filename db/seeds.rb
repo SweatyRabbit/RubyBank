2da83da43c2fe58c15a84a6899ee4f6bdd7b54a4
Ivan Hulko <ivan_hulko@epam.com>
2023-11-07 21:54:41 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-user = User.first_or_create(email: 'example@gmail.com', password: '123123', confirmed_at: Time.zone.now)
-CreditCard.create(number: FFaker::Bank.card_number, balance: 100_000, user:)
+emails = %w[example@gmail.com example1@gmail.com].freeze
 
-user1 = User.first_or_create(email: 'example2@gmail.com', password: '123123', confirmed_at: Time.zone.now)
-CreditCard.create(number: FFaker::Bank.card_number, balance: 100_000, user: user1)
+emails.each do |email|
+  User.find_or_create_by!(email:) do |user|
+    user.password = '123123'
+    user.confirmed_at = Time.zone.now
+    CreditCard.create!(number: FFaker::Bank.card_number, balance: 100_000, user:)
+  end
+end
